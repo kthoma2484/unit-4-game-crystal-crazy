624b8e146d338b6043e361dd305103c83ba410e4
KKT84 <35786891+kthoma2484@users.noreply.github.com>
2018-06-07 00:33:58 -0700

    completed game style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
     let sapphireVal = "";
     let rubyVal = "";    
     let gemVals = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+    let gemGameVals = [];
 
     // function to generate random number
     let ranGameNumber = function() {
@@ -14,20 +15,62 @@ $(document).ready(function() {
     }
 
     // function to generate random gem values
+    
+    
     let ranGemValues = function() {
         topazVal = "";
         emeraldVal = "";
         sapphireVal = "";
         rubyVal = "";
-        let aGemVal = gemVals[Math.floor(Math.random() * gemVals.length)];
-        
-    }
+        let gemGameVals = [];
+        let aGemVal = function() {
+            return gemVals[Math.floor(Math.random() * gemVals.length)];
+        }
+        let notHere = false;
 
+        do {
+            aGemVal();
+            if (topazVal == "") {
+            topazVal = aGemVal();
+            console.log('topaz value this game is ' + topazVal);
+            gemGameVals.push(topazVal);
+            console.log(gemGameVals);
+            }
+            
+            if (gemGameVals.indexOf(topazVal)) {
+                emeraldVal = aGemVal();
+                console.log('emerald value this game is ' + emeraldVal);
+                gemGameVals.push(emeraldVal);
+                console.log(gemGameVals);
+            }        
+
+            if (!gemGameVals.indexOf(topazVal) && !gemGameVals.indexOf(emeraldVal)) {
+                sapphireVal = aGemVal = gemVals[Math.floor(Math.random() * gemVals.length)];
+                console.log('sapphire value this game is ' + sapphireVal);
+                gemGameVals.push(sapphireVal);
+                console.log(gemGameVals);
+            }
+            
+            if (!gemGameVals.indexOf(topazVal) && !gemGameVals.indexOf(emeraldVal) && !gemGameVals.indexOf(sapphireVal)) {
+                rubyVal = gemVals[Math.floor(Math.random() * gemVals.length)]
+                console.log('ruby value this game is ' + rubyVal);
+                gemGameVals.push(rubyVal);
+                console.log(gemGameVals);
+            }
+        } 
+        while (topazVal == "" && emeraldVal == "" && sapphireVal == "" && rubyVal == "" );
+
+       
+     
+    }
 
 
     ranGameNumber();
     console.log('Random game number is = ' + ranGameNumber());
 
+   ranGemValues();
+   // console.log(ranGemValues())
+    
 $(document).click(function() {
 
     if ($('#topaz').data('clicked')) {
